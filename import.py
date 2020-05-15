baf29395f06f863bcb7373c0cba7a9ba0dde8977
Maria Beatriz de Vilhena <62449148+mbvilhena@users.noreply.github.com>
2020-05-15 21:43:09 +0100

    create.py now substitutes create.sql

--- import.py
+++ import.py
@@ -1,13 +1,19 @@
 import csv
 import os
 
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models import *
 
+app = Flask(__name__)
+
+# Set up database
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
+
     f = open("books.csv")
     reader = csv.reader(f)
     next(reader) #csvreader.__next__() - Return the next row of the reader’s iterable object as a list
